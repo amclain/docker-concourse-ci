b4434df8acbb3200fe5ad3a424fcdf393a102f37
Alex McLain <alex@alexmclain.com>
2016-10-20 22:48:55 -0700

    Add support for worker custom TSA port.

--- docker/worker/bootstrap.sh
+++ docker/worker/bootstrap.sh
@@ -2,6 +2,7 @@
 exec ./concourse_linux_amd64 worker \
   --work-dir /opt/concourse/worker \
   --tsa-host $TSA_HOST \
+  --tsa-port $TSA_PORT \
   --tsa-public-key /var/concourse/keys/host_key.pub \
   --tsa-worker-private-key /var/concourse/keys/worker_key \
   -- -dnsServer $DNS_SERVER
