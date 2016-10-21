3a04f262b213d5466374e8059c6198f4a6a9190e
Alex McLain <alex@alexmclain.com>
2016-10-20 22:23:47 -0700

    Formatting.

--- docker/worker/bootstrap.sh
+++ docker/worker/bootstrap.sh
@@ -3,5 +3,5 @@ exec ./concourse_linux_amd64 worker \
   --work-dir /opt/concourse/worker \
   --tsa-host $TSA_HOST \
   --tsa-public-key /var/concourse/keys/host_key.pub \
-  --tsa-worker-private-key /var/concourse/keys/worker_key\
+  --tsa-worker-private-key /var/concourse/keys/worker_key \
   -- -dnsServer $DNS_SERVER
